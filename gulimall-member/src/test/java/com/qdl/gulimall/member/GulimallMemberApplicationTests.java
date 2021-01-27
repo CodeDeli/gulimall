1aaa3294e89e99bfa64e0cfcae103563a179e6e5
deli <545029152@qq.com>
2021-01-27 16:08:43 +0800

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/qdl/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qdl.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gulimall-order/src/test/java/com/qdl/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qdl.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
